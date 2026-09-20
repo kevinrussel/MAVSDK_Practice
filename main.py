06d9a338161d9909f87a35800fe798534b07ce5c
Kevin Russel <kzone159@gmail.com>
2026-09-19 20:14:10 -0400

    adding code

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ async def run():
     drone = System()
     await drone.connect(system_address="udp://:14540")
     print("Waiting for drone...")
-    async for state in drone.core.connection_state():
-        if state.is_connected:
-            print("Connected!")
-            break
+    await drone.action.arm()
+    await drone.action.takeoff()
 
 asyncio.run(run())
